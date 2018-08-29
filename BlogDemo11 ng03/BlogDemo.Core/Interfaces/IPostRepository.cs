72c937cd5d32e65a791af5325b2741fae709b58f
Dave Young <solenovex@live.com>
2018-08-29 21:00:22 +0800

    update

---
+++ BlogDemo11 ng03/BlogDemo.Core/Interfaces/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlogDemo.Core.Entities;
+
+namespace BlogDemo.Core.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<PaginatedList<Post>> GetAllPostsAsync(PostParameters postParameters);
+        Task<Post> GetPostByIdAsync(int id);
+        void AddPost(Post post);
+        void Delete(Post post);
+        void Update(Post post);
+    }
+}
---
+++ BlogDemo11 ng03/blog-client/src/app/blog/blog.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { BlogRoutingModule } from './blog-routing.module';
+import { MaterialModule } from '../shared/material/material.module';
+import { BlogAppComponent } from './blog-app.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { PostService } from './services/post.service';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { AuthorizationHeaderInterceptor } from '../shared/oidc/authorization-header-interceptor.interceptor';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BlogRoutingModule,
+    HttpClientModule,
+    MaterialModule
+  ],
+  declarations: [
+    BlogAppComponent,
+    SidenavComponent,
+    ToolbarComponent,
+    PostListComponent
+  ],
+  providers: [
+    PostService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthorizationHeaderInterceptor,
+      multi: true
+    }
+  ]
+})
+export class BlogModule { }
